090e4f4f176d5e3a899498d941e939ecaef67de6
nkta <nkta1@hotmail.fr>
2020-03-26 17:01:11 +0100

    Add files via upload
    Ajout des fonctions liste des batchs et vérification de la fréquence

--- Calendrier_des_batchs/gestionDesBatchs.js
+++ Calendrier_des_batchs/gestionDesBatchs.js
@@ -1,7 +1,8 @@
 
 function liste() {
     //var jsonString = {"lBatchs":[{"col1":"cell in row 0, column 0","col2":"cell in row 0, column 1 "},{"col1":"cell in row 1, column 0","col2":"cell in row 1, column 1 "}]};
-    ListeBatch = {"lBatchs":[{"col1":"cell in row 0, column 0","col2":"cell in row 0, column 1 "},{"col1":"cell in row 1, column 0","col2":"cell in row 1, column 1 "}]}
+
+    ListeBatch = listeDesBatchs();
     // get the reference for the body
     var body = document.getElementsByTagName("body")[0];
 
@@ -10,17 +11,31 @@ function liste() {
     var tblBody = document.createElement("tbody");
 
     // creating all cells s
-    for (var i = 0; i < 2; i++) {
+    for (var i = 0; i < ListeBatch.lBatchs.length; i++) {
         // creates a table row
         var row = document.createElement("tr");
 
         var cell = document.createElement("td");
-        var cellText = document.createTextNode(ListeBatch.lBatchs[i].col1);
+        var cellText = document.createTextNode(ListeBatch.lBatchs[i].Nom);
         cell.appendChild(cellText);
         row.appendChild(cell);
 
         var cell = document.createElement("td");
-        var cellText = document.createTextNode(ListeBatch.lBatchs[i].col2);
+        var cellText = document.createTextNode(ListeBatch.lBatchs[i].Desc);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+        // add the row to the end of the table body
+        tblBody.appendChild(row);
+
+        var cell = document.createElement("td");
+        var cellText = document.createTextNode(ListeBatch.lBatchs[i].Freq);
+        cell.appendChild(cellText);
+        row.appendChild(cell);
+        // add the row to the end of the table body
+        tblBody.appendChild(row);
+
+        var cell = document.createElement("td");
+        var cellText = document.createTextNode(ListeBatch.lBatchs[i].Date);
         cell.appendChild(cellText);
         row.appendChild(cell);
         // add the row to the end of the table body
@@ -33,4 +48,181 @@ function liste() {
     body.appendChild(tbl);
     // sets the border attribute of tbl to 2;
     tbl.setAttribute("border", "2");
+}
+
+function verifFrequence(freq) {
+    switch(freq){
+        case 'journalier':
+            return true;
+        case 'hebdomadaire':
+        case 'mensuel':
+        case 'biannuel':
+        case 'annuel':
+        default :
+            return false
+    }
+}
+function listeDesBatchs() {
+    ListeBatch = {
+        "lBatchs": [
+            {
+                "Nom" : "pj_217",
+                "Desc" : "Export SIGP",
+                "Freq" :"journalier",
+                "Date" : "-"
+            },
+            {
+                "Nom" : "pj_224",
+                "Desc" : "Export EDU Quotidien",
+                "Freq" : "journalier",
+                "Date" : "-"
+            },
+            {
+                "Nom" : "pj_211",
+                "Desc" : "Export SIDIS",
+                "Freq" : "journalier",
+                "Date" : "-"
+            },
+            {
+                "Nom" : "pj_213",
+                "Desc" : "Export EDU",
+                "Freq" : "mensuel",
+                "Date" : "1"
+            },
+            {
+                "Nom" : "pj_214",
+                "Desc" : "Export Accidents Circulation SURF",
+                "Freq" : "mensuel",
+                "Date" : "1"
+            },
+            {
+                "Nom" : "pj_215",
+                "Desc" : "Export Accidents Métier SURF",
+                "Freq" : "mensuel",
+                "Date" : "16"
+            },
+            {
+                "Nom" : "pj_218",
+                "Desc" : "Export BSST",
+                "Freq" : "mensuel",
+                "Date" : "14"
+            },
+            {
+                "Nom" : "pj_221",
+                "Desc" : "Import Utilisations",
+                "Freq" : "mensuel",
+                "Date" : "14"
+            },
+            {
+                "Nom" : "pj_225",
+                "Desc" : "Envoi mail CE",
+                "Freq" : "mensuel",
+                "Date" : "1"
+            },
+            {
+                "Nom" : "pj_212",
+                "Desc" : "Export NEMO RCT",
+                "Freq" : "hebdomadaire",
+                "Date" : "mardi"
+            },
+            {
+                "Nom" : "pj_209",
+                "Desc" : "Import Absences SIGP",
+                "Freq" : "hebdomadaire",
+                "Date" : "dimanche"
+            },
+            {
+                "Nom" : "pj_210",
+                "Desc" : "Import Absences GEP",
+                "Freq" : "hebdomadaire",
+                "Date" : "dimanche"
+            },
+            {
+                "Nom" : "pj_201",
+                "Desc" : "Import Référentiels",
+                "Freq" : "hebdomadaire",
+                "Date" : "mercredi"
+            },
+            {
+                "Nom" : "pj_220",
+                "Desc" : "Import Fonctions Connexe",
+                "Freq" : "hebdomadaire",
+                "Date" : "mercredi"
+            },
+            {
+                "Nom" : "pj_202",
+                "Desc" : "Import Fonctions",
+                "Freq" : "hebdomadaire",
+                "Date" : "mercredi"
+            },
+            {
+                "Nom" : "pj_203",
+                "Desc" : "Import Entité Connexe",
+                "Freq" : "hebdomadaire",
+                "Date" : "mercredi"
+            },
+            {
+                "Nom" : "pj_204",
+                "Desc" : "Import Entité",
+                "Freq" : "hebdomadaire",
+                "Date" : "mercredi"
+            },
+            {
+                "Nom" : "pj_205",
+                "Desc" : "Import Agents Connexe",
+                "Freq" : "hebdomadaire",
+                "Date" : "mercredi"
+            },
+            {
+                "Nom" : "pj_208",
+                "Desc" : "Import Agents Sortis",
+                "Freq" : "hebdomadaire",
+                "Date" : "mercredi"
+            },
+            {
+                "Nom" : "pj_207",
+                "Desc" : "Import Agents Inactifs",
+                "Freq" : "hebdomadaire",
+                "Date" : "mercredi"
+            },
+            {
+                "Nom" : "pj_206",
+                "Desc" : "Import Agents actifs",
+                "Freq" : "hebdomadaire",
+                "Date" : "mercredi"
+            },
+            {
+                "Nom" : "pj_219",
+                "Desc" : "Import Historique Agents",
+                "Freq" : "hebdomadaire",
+                "Date" : "mercredi"
+            },
+            {
+                "Nom" : "pj_216",
+                "Desc" : "Export Accidents Métiers SURF",
+                "Freq" : "annuel",
+                "Date" : "01/02"
+            },
+            {
+                "Nom" : "pj_226",
+                "Desc" : "Export Accidents comparaison CNAM",
+                "Freq" : "annuel",
+                "Date" : "20/01"
+            },
+            {
+                "Nom" : "pj_227",
+                "Desc" : "Purge SIPREVA",
+                "Freq" : "biannuel",
+                "Date" : "01/05,01/11"
+            },
+            {
+                "Nom" : "pj_228",
+                "Desc" : "Purge SI-PENIB",
+                "Freq" : "biannuel",
+                "Date" : "01/05,01/11"
+            }
+
+        ]
+    };
+    return ListeBatch;
 }
